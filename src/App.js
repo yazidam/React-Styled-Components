c26fa6c82352c3dc467fa5c82568dd50e20d88a3
@ahmedyazid.mejri <ahmedyazid.mejri@esprit.tn>
2022-01-08 11:37:22 +0100

    Adding Theme

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
+import { ThemeProvider } from "styled-components";
+import Header from "./components/Header";
 import { Container } from "./components/styles/Container.styled";
 function App() {
+  const theme = {
+    colors: {
+      header: "#ebfbff",
+      body: "#fff",
+      footer: "#003333",
+    },
+  };
   return (
-    <Container>
-      <h1>hello world from ahmed</h1>
-    </Container>
+    <ThemeProvider theme={theme}>
+      <>
+        <Header />
+        <Container>
+          <h1>hello world from ahmed</h1>
+        </Container>
+      </>
+    </ThemeProvider>
   );
 }
 
---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { StyledHeader } from "./styles/Header.styled";
+
+const Header = () => {
+  return (
+    <StyledHeader>
+      <h1>Hubble</h1>
+    </StyledHeader>
+  );
+};
+
+export default Header;
