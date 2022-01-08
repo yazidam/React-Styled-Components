97d18bd34c445db6fad73a79d7127885bff20f7e
@ahmedyazid.mejri <ahmedyazid.mejri@esprit.tn>
2022-01-08 11:46:27 +0100

    Adding global style

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { ThemeProvider } from "styled-components";
 import Header from "./components/Header";
 import { Container } from "./components/styles/Container.styled";
+import GlobalStyles from "./components/styles/Global";
 function App() {
   const theme = {
     colors: {
@@ -12,6 +13,7 @@ function App() {
   return (
     <ThemeProvider theme={theme}>
       <>
+        <GlobalStyles />
         <Header />
         <Container>
           <h1>hello world from ahmed</h1>
