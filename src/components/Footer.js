effdf108c0cedcac54551f8d3113ffa5ff0276b7
@ahmedyazid.mejri <ahmedyazid.mejri@esprit.tn>
2022-01-13 11:23:02 +0100

    Adding scrolling to top

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,8 +3,13 @@ import SocialIcons from "./SocialIcons";
 import { Container } from "./styles/Container.styled";
 import { Flex } from "./styles/Flex.styled";
 import { StyledFooter } from "./styles/Footer.styled";
+import { FaArrowCircleUp } from "react-icons/fa";
+import styled from "styled-components";
 
 const Footer = () => {
+  const handleScroll = () => {
+    window.scroll({ top: 0, behavior: "smooth" });
+  };
   return (
     <StyledFooter>
       <Container>
@@ -32,6 +37,11 @@ const Footer = () => {
 
           <SocialIcons />
         </Flex>
+        <UpIcon>
+          <a onClick={handleScroll}>
+            <FaArrowCircleUp />
+          </a>
+        </UpIcon>
         <p>&copy; 2022 Huddle. All rights reserved</p>
       </Container>
     </StyledFooter>
@@ -39,3 +49,28 @@ const Footer = () => {
 };
 
 export default Footer;
+
+export const UpIcon = styled.div`
+  display: flex;
+
+  flex-direction: row-reverse;
+
+  a {
+    border: 1px solid #fff;
+    cursor: pointer;
+    border-radius: 50%;
+    color: #fff;
+    display: inline-flex;
+    align-items: center;
+    justify-content: center;
+    margin-right: 10px;
+    height: 40px;
+    width: 40px;
+    text-decoration: none;
+  }
+
+  @media (max-width: ${({ theme }) => theme.mobile}) {
+    justify-content: center;
+    margin-top: 20px;
+  }
+`;
