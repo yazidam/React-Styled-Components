0766ec29caec370d5c1744f03fdac187d3426c70
@ahmedyazid.mejri <ahmedyazid.mejri@esprit.tn>
2022-01-16 11:41:02 +0100

    Fix showing img in card

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,7 +3,11 @@ import { StyledCard } from "./styles/Card.styled";
 
 const Card = ({ item }) => {
   return (
-    <StyledCard data-aos="fade-right" data-aos-duration="1500">
+    <StyledCard
+      position={item.position}
+      data-aos="fade-right"
+      data-aos-duration="1500"
+    >
       <div>
         <h2>{item.title}</h2>
         <p>{item.body}</p>
--- src/components/styles/Card.styled.js
+++ src/components/styles/Card.styled.js
@@ -8,6 +8,7 @@ export const StyledCard = styled.div`
   box-shadow: 0 0 10px rgba(0, 0, 0, 0.15);
   margin: 40px 0;
   padding: 60px;
+  flex-direction: ${({ position }) => (position ? "row-reverse" : "row")};
 
   img {
     width: 80%;
@@ -19,14 +20,4 @@ export const StyledCard = styled.div`
   @media (max-width: ${({ theme }) => theme.mobile}) {
     flex-direction: column;
   }
-`;
-
-export const ShowCard = styled.div`
-  display: flex;
-  align-items: center;
-  background-color: #fff;
-  border-radius: 15px;
-  box-shadow: 0 0 10px rgba(0, 0, 0, 0.15);
-  margin: 40px 0;
-  padding: 60px;
 `;
