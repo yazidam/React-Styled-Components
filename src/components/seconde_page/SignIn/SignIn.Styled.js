8649e01aa9ac22f5da40834df4fec62ea1b1da64
@ahmedyazid.mejri <ahmedyazid.mejri@esprit.tn>
2022-01-17 20:30:51 +0100

    Fix width

--- src/components/seconde_page/SignIn/SignIn.Styled.js
+++ src/components/seconde_page/SignIn/SignIn.Styled.js
@@ -63,8 +63,9 @@ export const Form = styled.form`
   padding: 80px 32px;
   border-radius: 4px;
   box-shadow: 0 1px 3px rgba(0, 0, 0, 0.9);
-  @media screen and (max-width: 490px) {
+  @media screen and (max-width: 768px) {
     padding: 32px 32px;
+    max-width: 210px;
   }
 `;
 
